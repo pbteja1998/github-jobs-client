0bf4f9eb0e209b1d60d55aba7d105f91fcb54c3d
Bhanu Teja P <pbteja1998@gmail.com>
2020-10-16 19:34:15 +0530

    Add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { QueryCache, ReactQueryCacheProvider } from 'react-query'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { App } from '../components'
 const queryCache = new QueryCache()
 
@@ -8,6 +9,34 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ReactQueryCacheProvider queryCache={queryCache}>
       <App>
+        <Head>
+          <meta charSet='utf-8' />
+          <meta name='application-name' content='GitHub Jobs Clone' />
+          <meta name='twitter:card' content='summary' />
+          <meta name='twitter:creator' content='@pbteja1998' />
+          <meta property='og:type' content='website' />
+          <meta name='author' content='Bhanu Teja P' />
+          <meta property='og:site_name' content='GitHub Jobs Clone' />
+          <meta name='theme-color' content='#5964E0' />
+          <meta
+            name='description'
+            content='A GitHub Jobs clone made using the design from frontendmentor.io.'
+          />
+          <meta property='og:title' content='GitHub Jobs Clone' />
+          <meta
+            property='og:description'
+            content='A GitHub Jobs clone made using the design from frontendmentor.io.'
+          />
+          <meta property='og:url' content='https://jobs.bhanuteja.dev' />
+          <meta property='twitter:title' content='GitHub Jobs' />
+          <meta
+            property='twitter:description'
+            content='A GitHub Jobs clone made using the design from frontendmentor.io.'
+          />
+          <meta property='twitter:url' content='https://jobs.bhanuteja.dev' />
+          <meta property='og:image' content='/preview.jpg' />
+          <meta property='twitter:image' content='/preview.jpg' />
+        </Head>
         <Component {...pageProps} />
       </App>
     </ReactQueryCacheProvider>
