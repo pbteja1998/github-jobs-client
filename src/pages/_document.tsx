dceefabaeea434858a7b1069dd6030cb7b1c43bf
Bhanu Teja P <pbteja1998@gmail.com>
2020-10-16 19:37:59 +0530

    Revert "Add meta tags"
    This reverts commit 0bf4f9eb0e209b1d60d55aba7d105f91fcb54c3d.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,32 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <meta charSet='utf-8' />
+          <meta name='application-name' content='GitHub Jobs Clone' />
+          <meta name='twitter:card' content='summary' />
+          <meta name='twitter:creator' content='@pbteja1998' />
+          <meta property='og:type' content='website' />
+          <meta name='author' content='Bhanu Teja P' />
+          <meta property='og:site_name' content='GitHub Jobs Clone' />
+          <meta name='theme-color' content='#5964E0' />
+          <meta
+            name='description'
+            content='A GitHub Jobs clone made using the design from frontendmentor.io.'
+          />
+          <meta property='og:title' content='GitHub Jobs Clone' />
+          <meta
+            property='og:description'
+            content='A GitHub Jobs clone made using the design from frontendmentor.io.'
+          />
+          <meta property='og:url' content='https://jobs.bhanuteja.dev' />
+          <meta property='twitter:title' content='GitHub Jobs' />
+          <meta
+            property='twitter:description'
+            content='A GitHub Jobs clone made using the design from frontendmentor.io.'
+          />
+          <meta property='twitter:url' content='https://jobs.bhanuteja.dev' />
+          <meta property='og:image' content='/preview.jpg' />
+          <meta property='twitter:image' content='/preview.jpg' />
           <link rel='shortcut icon' href='/favicon.ico' />
           <link
             href='https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&display=swap'
