5c3591a8e9015f2064990e5c702f67c3dd67f638
Bhanu Teja P <pbteja1998@gmail.com>
2020-10-16 06:01:46 +0530

    Add location and full time filters in mobile

---
+++ src/components/Modal.tsx
@@ -0,0 +1,55 @@
+import { Transition } from '@headlessui/react'
+import { ReactNode, useState } from 'react'
+
+export default function Modal({
+  className = '',
+  children,
+  isOpen,
+  setIsOpen,
+}: {
+  className?: string
+  children?: ReactNode
+  isOpen: boolean
+  setIsOpen: (val: boolean) => void
+}) {
+  if (!isOpen) {
+    return <></>
+  }
+  return (
+    <div className={className}>
+      <div className='fixed inset-0 z-10 overflow-y-auto'>
+        <div className='flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center'>
+          <Transition
+            show={isOpen}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            className='fixed inset-0 transition-opacity'
+          >
+            <div className='absolute inset-0 bg-gray-500 opacity-75'></div>
+          </Transition>
+          <span className='inline-block h-screen align-middle'></span>
+          &#8203;
+          <Transition
+            show={isOpen}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            enterTo='opacity-100 translate-y-0 sm:scale-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+            leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            className='inline-block overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl w-76'
+            role='dialog'
+            aria-modal='true'
+            aria-labelledby='modal-headline'
+          >
+            {children}
+          </Transition>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from 'react'
 import { useMutation } from 'react-query'
-import { Button, JobView, Input, JobViewSkeleton } from '../components'
+import { Button, JobView, Input, JobViewSkeleton, Modal } from '../components'
 import { Job } from '../types'
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false)
+
   const [description, setDescription] = useState('')
   const [location, setLocation] = useState('')
   const [fullTime, setFullTime] = useState(false)
 
-  const [finalDescription, setFinalDescription] = useState('')
-  const [finalLocation, setFinalLocation] = useState('')
-  const [finalFullTime, setFinalFullTime] = useState(false)
+  const [final, setFinal] = useState({
+    finalDescription: '',
+    finalLocation: '',
+    finalFullTime: false,
+  })
 
   const [mutate, { isLoading, error, data: jobs }] = useMutation(() =>
     fetch(
       `https://api.allorigins.win/get?url=${encodeURIComponent(
-        `https://jobs.github.com/positions.json?page=1&description=${finalDescription}&location=${finalLocation}&full_time=${finalFullTime}`
+        `https://jobs.github.com/positions.json?page=1&description=${final.finalDescription}&location=${final.finalLocation}&full_time=${final.finalFullTime}`
       )}`
     )
       .then((res) => res.json())
@@ -24,12 +28,62 @@ export default function Home() {
 
   useEffect(() => {
     mutate()
-  }, [finalDescription, finalLocation, finalFullTime])
+    setDescription('')
+    setLocation('')
+    setFullTime(false)
+  }, [final])
 
   if (error) return 'An error has occurred.'
 
   return (
     <>
+      <Modal className='md:hidden' isOpen={modalOpen} setIsOpen={setModalOpen}>
+        <Input
+          icon={
+            <svg width='17' height='24' xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M14.358 2.451A8.3 8.3 0 008.448 0a8.3 8.3 0 00-5.911 2.451c-2.922 2.925-3.285 8.427-.786 11.76l6.697 9.683 6.687-9.669c2.508-3.347 2.145-8.85-.777-11.774zm-5.833 8.894a3.057 3.057 0 01-3.051-3.054 3.057 3.057 0 013.05-3.055 3.057 3.057 0 013.052 3.055 3.057 3.057 0 01-3.051 3.054z'
+                fill='#5964E0'
+                fillRule='nonzero'
+              />
+            </svg>
+          }
+          className='flex pl-1 border-b border-dark-grey border-opacity-20 md:hidden'
+          label='Location Filter'
+          placeholder='Filter by location...'
+          value={location}
+          setValue={setLocation}
+        />
+        <Input
+          className='flex pl-1 md:hidden rounded-r-md'
+          label='Full Time'
+          isCheckbox={true}
+          checkboxValue={fullTime}
+          setCheckboxValue={setFullTime}
+        >
+          <>
+            <p className='ml-4 text-base font-bold font-brand leading-button text-very-dark-blue dark:text-white'>
+              Full Time Only
+            </p>
+          </>
+        </Input>
+        <div className='px-6 pb-6'>
+          <Button
+            primary={true}
+            className='w-full'
+            onClick={() => {
+              setFinal({
+                finalDescription: description,
+                finalLocation: location,
+                finalFullTime: fullTime,
+              })
+              setModalOpen(false)
+            }}
+          >
+            Search
+          </Button>
+        </div>
+      </Modal>
       <div className='grid justify-center max-w-full grid-cols-1 px-6 mx-auto transform -translate-y-1/2 md:grid-cols-3'>
         <Input
           icon={
@@ -48,7 +102,7 @@ export default function Home() {
           setValue={setDescription}
         >
           <div className='flex items-center md:hidden'>
-            <div>
+            <div onClick={() => setModalOpen(true)}>
               <svg width='20' height='20' xmlns='http://www.w3.org/2000/svg'>
                 <path
                   className='fill-current text-dark-grey dark:text-white'
@@ -58,7 +112,16 @@ export default function Home() {
                 />
               </svg>
             </div>
-            <div className='ml-6 inline-flex items-center justify-center w-12 h-12 p-3.5 bg-violet'>
+            <div
+              className='ml-6 inline-flex items-center justify-center w-12 h-12 p-3.5 bg-violet'
+              onClick={() => {
+                setFinal({
+                  finalDescription: description,
+                  finalLocation: location,
+                  finalFullTime: fullTime,
+                })
+              }}
+            >
               <svg width='20' height='20' xmlns='http://www.w3.org/2000/svg'>
                 <path
                   d='M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z'
@@ -101,15 +164,93 @@ export default function Home() {
               primary={true}
               className='ml-auto mr-2'
               onClick={() => {
-                setFinalDescription(description)
-                setFinalLocation(location)
-                setFinalFullTime(fullTime)
+                setFinal({
+                  finalDescription: description,
+                  finalLocation: location,
+                  finalFullTime: fullTime,
+                })
               }}
             >
               Search
             </Button>
           </>
         </Input>
+      </div>
+
+      <div className='mb-6 -mt-6 space-x-2 text-center'>
+        {final.finalDescription && (
+          <span className='inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-violet text-white'>
+            {final.finalDescription}
+            <button
+              type='button'
+              className='flex-shrink-0 -mr-0.5 ml-1.5 inline-flex text-white focus:outline-none focus:text-white hover:text-opacity-75 focus:text-opacity-75'
+              aria-label='Remove large badge'
+              onClick={() => setFinal({ ...final, finalDescription: '' })}
+            >
+              <svg
+                className='w-2 h-2'
+                stroke='currentColor'
+                fill='none'
+                viewBox='0 0 8 8'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeWidth='1.5'
+                  d='M1 1l6 6m0-6L1 7'
+                />
+              </svg>
+            </button>
+          </span>
+        )}
+        {final.finalLocation && (
+          <span className='inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-violet text-white'>
+            {final.finalLocation}
+            <button
+              type='button'
+              className='flex-shrink-0 -mr-0.5 ml-1.5 inline-flex text-white focus:outline-none focus:text-white hover:text-opacity-75 focus:text-opacity-75'
+              aria-label='Remove large badge'
+              onClick={() => setFinal({ ...final, finalLocation: '' })}
+            >
+              <svg
+                className='w-2 h-2'
+                stroke='currentColor'
+                fill='none'
+                viewBox='0 0 8 8'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeWidth='1.5'
+                  d='M1 1l6 6m0-6L1 7'
+                />
+              </svg>
+            </button>
+          </span>
+        )}
+
+        {final.finalFullTime && (
+          <span className='inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-violet text-white'>
+            Only Full Time
+            <button
+              type='button'
+              className='flex-shrink-0 -mr-0.5 ml-1.5 inline-flex text-white focus:outline-none focus:text-white hover:text-opacity-75 focus:text-opacity-75'
+              aria-label='Remove large badge'
+              onClick={() => setFinal({ ...final, finalFullTime: false })}
+            >
+              <svg
+                className='w-2 h-2'
+                stroke='currentColor'
+                fill='none'
+                viewBox='0 0 8 8'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeWidth='1.5'
+                  d='M1 1l6 6m0-6L1 7'
+                />
+              </svg>
+            </button>
+          </span>
+        )}
       </div>
 
       <div className='grid grid-cols-1 px-6 pt-4 gap-x-3 xl:gap-x-8 gap-y-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
