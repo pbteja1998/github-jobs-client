a29e5bb3f8bed4088f822cdc4e2b125e54e22907
Bhanu Teja P <pbteja1998@gmail.com>
2020-10-24 05:31:40 +0530

    Add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { App } from '../components'
 const queryCache = new QueryCache()
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(2058536, 6)
+  }, [])
   return (
     <ReactQueryCacheProvider queryCache={queryCache}>
       <App>
